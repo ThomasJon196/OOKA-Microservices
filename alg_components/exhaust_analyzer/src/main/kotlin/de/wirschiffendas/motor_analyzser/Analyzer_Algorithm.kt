4da307bfc41e91e28a05eeb308246887e9f0bb45
Thomas Jonas <thomas.jonas@inovex.de>
2023-09-13 13:23:48 +0200

    Fix: Analyzer kafka topic

--- alg_components/exhaust_analyzer/src/main/kotlin/de/wirschiffendas/motor_analyzser/Analyzer_Algorithm.kt
+++ alg_components/exhaust_analyzer/src/main/kotlin/de/wirschiffendas/motor_analyzser/Analyzer_Algorithm.kt
@@ -8,86 +8,54 @@ import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class ExhaustAnalyzer(val status_notification_fun: (String, String, String) -> Unit = { _, _, _ -> }) {
+class ExhaustAnalyzer(
+        val status_notification_fun: (String, String, String) -> Unit = { _, _, _ -> }
+) {
     val logger: Logger = LoggerFactory.getLogger(ExhaustAnalyzer::class.java)
 
     // Execute analysis for each equipment in parallel via coroutines
     fun executeAnalysis(configuration: Configuration) = runBlocking {
         logger.info("Executing analysis...")
 
-        val gearboxAnalyzer = GearboxAnalyzer("GEARBOX", status_notification_fun, logger)
-        val engineAnalyzer = EngineAnalyzer("ENGINE", status_notification_fun, logger)
-        val startingSystemAnalyzer =
-                StartingSystemAnalyzer("STARTING_SYSTEM", status_notification_fun, logger)
+        val gearboxAnalyzer = MonitoringAnalyzer("TEST_MONITORING", status_notification_fun, logger)
+        val engineAnalyzer = GearboxAnalyzer("TEST_GEARBOX", status_notification_fun, logger)
 
         // Start all analyzers in parallel
         launch(Dispatchers.Default) { gearboxAnalyzer.executeAnalysis(configuration) }
-
         launch(Dispatchers.Default) { engineAnalyzer.executeAnalysis(configuration) }
+    }
+}
 
-        launch(Dispatchers.Default) { startingSystemAnalyzer.executeAnalysis(configuration) }
+class MonitoringAnalyzer(
+        val equipmentName: String,
+        val status_notification_fun: (id: String, equipmentName: String, status: String) -> Unit,
+        val logger: Logger
+) {
+    /* Simulated analyzer. Would contain complex algorithm. */
+    var status: Status = Status.NOT_STARTED
+
+    suspend fun updateStatus(configuration: Configuration, newStatus: Status) {
+        val statusString = newStatus.toString()
+        status_notification_fun(configuration.request_id, equipmentName, statusString)
+        status = newStatus
+    }
+
+    suspend fun executeAnalysis(configuration: Configuration) {
+        updateStatus(configuration, Status.RUNNING)
+
+        delay(2000) // Simulate computation time
+
+        if (randomlyFail()) {
+            logger.info("Failed!")
+            updateStatus(configuration, Status.FAILED)
+        } else {
+            logger.info("Success! (70% probability)")
+            updateStatus(configuration, Status.SUCCESS)
+        }
     }
 }
 
 class GearboxAnalyzer(
-        val equipmentName: String,
-        val status_notification_fun: (id: String, equipmentName: String, status: String) -> Unit,
-        val logger: Logger
-) {
-    /* Simulated analyzer. Would contain complex algorithm. */
-    var status: Status = Status.NOT_STARTED
-
-    suspend fun updateStatus(configuration: Configuration, newStatus: Status) {
-        val statusString = newStatus.toString()
-        status_notification_fun(configuration.request_id, equipmentName, statusString)
-        status = newStatus
-    }
-
-    suspend fun executeAnalysis(configuration: Configuration) {
-        updateStatus(configuration, Status.RUNNING)
-
-        delay(2000) // Simulate computation time
-
-        if (randomlyFail()) {
-            logger.info("Failed!")
-            updateStatus(configuration, Status.FAILED)
-        } else {
-            logger.info("Success! (70% probability)")
-            updateStatus(configuration, Status.SUCCESS)
-        }
-    }
-}
-
-class EngineAnalyzer(
-        val equipmentName: String,
-        val status_notification_fun: (id: String, equipmentName: String, status: String) -> Unit,
-        val logger: Logger
-) {
-    /* Simulated analyzer. Would contain complex algorithm. */
-    var status: Status = Status.NOT_STARTED
-
-    suspend fun updateStatus(configuration: Configuration, newStatus: Status) {
-        val statusString = newStatus.toString()
-        status_notification_fun(configuration.request_id, equipmentName, statusString)
-        status = newStatus
-    }
-
-    suspend fun executeAnalysis(configuration: Configuration) {
-        updateStatus(configuration, Status.RUNNING)
-
-        delay(2000) // Simulate computation time
-
-        if (randomlyFail()) {
-            logger.info("Failed!")
-            updateStatus(configuration, Status.FAILED)
-        } else {
-            logger.info("Success! (70% probability)")
-            updateStatus(configuration, Status.SUCCESS)
-        }
-    }
-}
-
-class StartingSystemAnalyzer(
         val equipmentName: String,
         val status_notification_fun: (id: String, equipmentName: String, status: String) -> Unit,
         val logger: Logger
